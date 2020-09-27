0f2a1a96ee5160151b1dce3ad16c37676c353991
eddie <aigagror@gmail.com>
2020-09-26 23:49:42 -0500

    removed the group class

--- gym_go/govars.py
+++ gym_go/govars.py
@@ -9,21 +9,3 @@ PASS_CHNL = 4
 DONE_CHNL = 5
 
 NUM_CHNLS = 6
-
-
-class Group:
-    def __init__(self):
-        self.locations = set()
-        self.liberties = set()
-
-    def copy(self):
-        groupcopy = Group()
-        groupcopy.locations = self.locations.copy()
-        groupcopy.liberties = self.liberties.copy()
-        return groupcopy
-
-    def __str__(self):
-        return f'{self.locations}LOC {self.liberties}LIB'
-
-    def __repr__(self):
-        return self.__str__()
