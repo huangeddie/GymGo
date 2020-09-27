01ab5ec18e6dee36aabce0fded5f1cf841f066b1
eddie <aigagror@gmail.com>
2020-09-26 21:22:59 -0500

    simplified demo

--- demo.py
+++ demo.py
@@ -4,7 +4,6 @@ import gym
 
 # Arguments
 parser = argparse.ArgumentParser(description='Demo Go Environment')
-parser.add_argument('--randai', action='store_true')
 parser.add_argument('--boardsize', type=int, default=7)
 parser.add_argument('--komi', type=float, default=0)
 args = parser.parse_args()
@@ -15,17 +14,16 @@ go_env = gym.make('gym_go:go-v0', size=args.boardsize, komi=args.komi)
 # Game loop
 done = False
 while not done:
-    action = go_env.render(mode="human")
-    if action == -1:
-        exit()
-    try:
-        _, _, done, _ = go_env.step(action)
-    except Exception as e:
-        print(e)
-        continue
-    if args.randai:
-        if go_env.game_ended():
-            break
-        action = go_env.uniform_random_action()
-        _, _, done, _ = go_env.step(action)
-go_env.render(mode="human")
+    go_env.render(mode="terminal")
+    move = input("Input move '(row col)/p': ")
+    if move == 'p':
+        action = None
+    else:
+        action = int(move[0]), int(move[2])
+    state, reward, done, info = go_env.step(action)
+
+    if go_env.game_ended():
+        break
+    action = go_env.uniform_random_action()
+    state, reward, done, info = go_env.step(action)
+go_env.render(mode="terminal")
