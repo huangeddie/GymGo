a562139f78855cfd926c6faabe4ead18b91a2a52
eddie <aigagror@gmail.com>
2020-10-02 15:53:18 -0500

    refactor

--- gym_go/gogame.py
+++ gym_go/gogame.py
@@ -327,7 +327,8 @@ def batch_canonical_form(batch_state):
 
     for i in white_players_idcs:
         batch_state[i] = batch_state[i, channels]
-        batch_state[i, govars.TURN_CHNL] = 1 - batch_player[i]
+
+    batch_state[white_players_idcs, govars.TURN_CHNL] = 1 - batch_player[white_players_idcs]
 
     return batch_state
 
