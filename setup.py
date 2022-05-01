83648efe9a6524c7fb2a13e4b6bad9f18720f88c
Rohan Mitchell <rohan@rohanmitchell.com>
2022-05-01 12:21:44 +1000

    Bump gym version to 0.23.1

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='gym_go',
     version='0.0.1',
-    install_requires=['gym==0.22.0']  # and other dependencies
+    install_requires=['gym==0.23.1']  # and other dependencies
 )
